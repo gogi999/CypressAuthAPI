e558d0bbed8c109087660a6769960809d94d3833
gogi999 <g.petrovic9@gmail.com>
2024-08-04 12:01:20 +0200

    Added positive and negative test case

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,8 +1,24 @@
 /// <reference types="cypress" />
 
 it('Returns 200 when we hit /register', () => {
-  cy.request('POST', 'http://localhost:3000/api/user/register')
+  let body = {
+    name: 'TestName',
+    email: 'foo@bar.com',
+    password: 'Test09876'
+  }
+
+  cy.request('POST', 'http://localhost:3000/api/user/register', body)
     .then((response) => {
       expect(response.status).to.eq(200)
     })
+})
+
+it('Returns 400 when we hit /register with no body', () => {
+  cy.request({
+    method: 'POST',
+    url: 'http://localhost:3000/api/user/register',
+    failOnStatusCode: false
+  }).then((response) => {
+    expect(response.status).to.eq(400)
+  })
 })
