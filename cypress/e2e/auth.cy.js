3a614ac25f25a4ef664ba801ca6edaa9a787d56f
gogi999 <g.petrovic9@gmail.com>
2024-08-05 11:12:16 +0200

    Making more robust tests and removing duplicates

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,24 +1,31 @@
 /// <reference types="cypress" />
 
-it('Returns 200 when we hit /register', () => {
-  let body = {
-    name: 'TestName',
-    email: 'foo@bar.com',
-    password: process.env.PASSWORD
-  }
-
-  cy.request('POST', 'http://localhost:3000/api/user/register', body)
-    .then((response) => {
-      expect(response.status).to.eq(200)
-    })
-})
-
-it('Returns 400 when we hit /register with no body', () => {
-  cy.request({
-    method: 'POST',
-    url: 'http://localhost:3000/api/user/register',
-    failOnStatusCode: false
-  }).then((response) => {
-    expect(response.status).to.eq(400)
+describe('/user/register', () => {
+  const registerEndpoint = 'http://localhost:3000/api/user/register'
+  it('Register creates user', () => {
+    let body = {
+      name: 'TestName',
+      email: 'foo@bar.com',
+      password: 'Test09876' // process.env.PASSWORD
+    }
+  
+    cy.request('POST', registerEndpoint, body)
+      .then((response) => {
+        expect(response.status).to.eq(200)
+        expect(response.body.name).to.eq('TestName')
+        expect(response.body.email).to.eq('foo@bar.com')
+        expect(response.body.password).to.eq('Test09876')
+      })
   })
+  
+  it('Returns 400 when we hit /register with no body', () => {
+    cy.request({
+      method: 'POST',
+      url: registerEndpoint,
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(400)
+    })
+  })
+  
 })
