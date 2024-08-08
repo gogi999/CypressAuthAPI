749ee5c176a0fa3b455f4fc39e165af551aa5dd7
gogi999 <g.petrovic9@gmail.com>
2024-08-08 10:11:56 +0200

    Added login endpoint

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -19,7 +19,7 @@ describe('/user/register', () => {
     let body = {
       name: 'TestUser',
       email: dynamicEmail,
-      password: process.env.PASSWORD
+      password: 'pass123456' // process.env.PASSWORD
     }
   
     cy.request('POST', registerEndpoint, body)
@@ -35,7 +35,7 @@ describe('/user/register', () => {
     let badTestUser = {
       name: '1',
       email: 'testuser',
-      password: process.env.PASSWORD
+      password: '1' // process.env.PASSWORD
     }
   
     cy.request({
@@ -52,7 +52,7 @@ describe('/user/register', () => {
     let badTestUser = {
       name: 'ValidName',
       email: 'invalidEmail',
-      password: process.env.PASSWORD 
+      password: 'validPassword' // process.env.PASSWORD
     }
   
     cy.request({
@@ -70,7 +70,7 @@ describe('/user/register', () => {
     let goodTestUser = {
       name: 'ValidName',
       email: 'doNotDeleteEmail@email.com',
-      password: process.env.PASSWORD 
+      password: 'validPassword' // process.env.PASSWORD
     }
   
     cy.request({
--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const User = require('../model/users');
 const { DataValidation } = require('../dataValidation');
 
 router.post('/register', async (req, res) => {
-    // Validae that the body content matches our requirements
+    // Validate that the body content matches our requirements
     const { error } = DataValidation.registerValidation(req.body);
 
     if (error) return res.status(400).send(error.details[0].message);
@@ -29,6 +29,24 @@ router.post('/register', async (req, res) => {
     } catch (err) {
         res.status(400).send(err);
     }
+});
+
+router.post('/login', async (req, res) => {
+    // This will return only the error part instead of the entire object
+    const { error } = DataValidation.loginValidation(req.body);
+
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // Check if the email exists
+    const userInDB = await User.findOne({ email: req.body.email });
+
+    if (!userInDB) return res.status(400).send('Email not registered!');
+
+    const validPassword = await bcrypt.compare(req.body.password, userInDB.password);
+
+    if (!validPassword) return res.status(401).send('Invalid password!');
+
+    res.status(200).send('Logged in!');
 });
 
 module.exports = router;
